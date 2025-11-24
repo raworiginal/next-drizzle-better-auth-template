af7c669fb8c805937261e04b9e1a68f14d36d1fc
raworiginal <118763950+raworiginal@users.noreply.github.com>
2025-11-24 14:06:55 -0800

    removed background color on hero in landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
 	return (
 		<main className="hero bg-base-200 min-h-screen">
-			<div className="hero-content bg-base-100 text-center ">
+			<div className="hero-content text-center ">
 				<div className="max-w-md">
 					<h1 className="text-5xl font-bold">Hello there</h1>
 					<p className="py-6 text-left">
